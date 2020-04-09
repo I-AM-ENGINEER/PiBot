994131dc402349f02be1725a751f6bafbcd7eae6
I-AM-ENGINEER <61810711+I-AM-ENGINEER@users.noreply.github.com>
2020-04-09 23:34:05 +1100

    Revert "Merge branch 'master' of https://github.com/I-AM-ENGINEER/PiBot"
    This reverts commit 8ea4e5c18f248106b9e0a98a105679d0f098cdd1, reversing
    changes made to f20f05dfe99e01c3a8f3a61b2af1dad5c00a33e3.

--- www/index.php
+++ www/index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang=ru>
-
-
+<HTML lang=ru>
+	
+	
 
 	<head>
 		<meta charset="UTF-8">
 		<title>БОТОУПРАВЛЯТЕЛЬ9000</title>
-		<!-- Bootstrap links -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="mainStyle.css"/>
+		<link rel="stylesheet" href="test.css">
 	</head>
 
 	<body>
@@ -42,43 +39,45 @@
 				fclose($f);
 			}
 		?>
+		
 
 
+		<center><H1>БОТОУПРАВЛЯТЕЛЬ9000</H1></center>
+		
 
+		
 
-		<div class="container-fluid">
-			<div class="row p-0">
-				<div class="col-sm-12 col-md-8 col-lg-7 p-0">
-					<img class="videoCamera" src="http://sdwad5.ru:8081" alt="Video from camera" />
-				</div>
-				<div class="col bg-dark">
-					<div class="row">
-						<div class="col-xl-2 col-lg-3 col-md-5 col-sm-12 border-right">
-							<form action="index.php" method="post">
-									<div class="row">
-										<div class="col col-md-12 col-lg-12 p-1">
-											<button class="btn btn-light btn-block" type="submit" name="FORWARD"><img class="img-fluid forward" src="src/arrow.png"/></button>
-										</div>
-										<div class="col col-md-12 col-lg-12 p-1">
-											<button class="btn btn-light btn-block" type="submit" name="RIGHT"><img class="img-fluid right" src="src/arrow.png"/></button>
-										</div>
-										<div class="col col-md-12 col-lg-12 p-1">
-											<button class="btn btn-light btn-block" type="submit" name="LEFT"><img class="img-fluid left" src="src/arrow.png"/></button>
-										</div>
-										<div class="col col-md-12 col-lg-12 p-1">
-											<button class="btn btn-light btn-block" type="submit" name="BACKWARD"><img class="img-fluid backward" src="src/arrow.png"/></button>
-										</div>
-									</div>
-
-							  <button class="col-12 col-lg btn btn-success mb-1 btn-block" type="submit" name="ENABLE">ENABLE</button>
-							  <button class="col-12 col-lg btn btn-secondary mb-1 btn-block" type="submit" name="TO_HOME">TO_HOME</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<br>
+		<form action="index.php" method="post">
+			<table align="center" width="320" height="200" style="font-size: 30px">
+				<colgroup>
+					<col span="3" style="background:Khaki">
+					<col style="background-color:LightCyan">
+				</colgroup>
+				<tr>
+					<th colspan="3">Управление</th>
+				</tr>
+				<tr>
+					<th colspan="3"><input type="submit" name="FORWARD" value="FORWARD"/></th>
+				</tr>
+				<tr>
+					<th><input type="submit" name="LEFT" value="LEFT" /></th>
+					<th>_______</th>
+					<th><input type="submit" name="RIGHT" value="RIGHT" /></th>
+				</tr>
+				<tr>
+					<th colspan="3"><input type="submit" name="BACKWARD" value="BACKWARD" /></th>
+				</tr>
+				<tr>
+						
+				</tr>
+			</table>
+			
+			<center><img src="http://sdwad5.ru:8081" alt="Video from camera" /></center>
+			
+		    <input type="submit" name="ENABLE" value="ENABLE" />
+		    <input type="submit" name="TO_HOME" value="TO_HOME" />
+		</form>
+		</br>
 	</body>
-</html>
+</HTMP>
